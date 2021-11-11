9dc4ebcffe3dee8c184bb9fea4c1b69b22b95bca
Kuhy <kwh1019_@naver.com>
2021-11-12 02:19:58 +0900

    Feat : Place Model, View, Kakao_place_crawl 구현

---
+++ place/kakao_selenium.py
@@ -0,0 +1,72 @@
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+import os.path
+
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+
+
+class kakaoCrawler():
+    def __init__(self):
+        # 드라이버 자동 설치
+        chromedriver_autoinstaller.install(True)
+        option = webdriver.ChromeOptions()
+        option.add_argument("headless")
+        option.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(
+            os.getcwd() + f'\{chrome_ver}\chromedriver.exe', options=option)
+        self.driver.implicitly_wait(5)
+
+    # Get place info
+
+    def place_detail_crawl(self, id):
+        # access
+        url = 'https://place.map.kakao.com/'
+        self.driver.get(url + str(id))
+        kakaoPlaceID = id
+        name = self.driver.find_elements_by_class_name("tit_location")[1].text
+
+        # category
+        try:
+            category = self.driver.find_element_by_class_name(
+                "txt_location").text
+        except:
+            category = ""
+        # location
+        try:
+            location = self.driver.find_element_by_class_name(
+                "txt_address").text
+        except:
+            location = ""
+        # businessHours
+        try:
+            businessHours = self.driver.find_element_by_class_name(
+                "txt_operation").text + self.driver.find_element_by_class_name("time_operation").text
+        except:
+            businessHours = ""
+        # menu
+        try:
+            menu = []
+            self.driver.find_element_by_class_name("open_txt").click()
+            menuName = self.driver.find_elements_by_class_name("loss_word")
+            menuName = [i.text for i in menuName]
+            priceData = self.driver.find_elements_by_class_name("price_menu")
+            price = [i.text for i in priceData]
+            for m, p in zip(menuName, price):
+                menu.append({"name": m, "price": p})
+        except:
+            menu = []
+
+        res = {
+            'kakaoPlaceID': kakaoPlaceID,
+            'name': name,
+            'category': category,
+            'location': location,
+            'businessHours': businessHours,
+            # 'description': description,
+            # 'imageSrc': imageSrc,
+            'menu': menu,
+        }
+        return res
--- place/views.py
+++ place/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Place, Menu, RegionFestival
+from django.db.models import Q
+import json
+import kakao_selenium
 
-# Create your views here.
+
+def get_place_detail(request, place_name):
+    place = Place.objects.filter(Q(title=place_name))
+    return render(request, 'place.html', {'place': place})
+
+
+def get_regionFestival_detail(request, place_name):
+    RegionFestival = RegionFestival.objects.filter(Q(title=place_name))
+    return render(request, 'regionfestival.html', {'RegionFestival': RegionFestival})
+
+
+def set_place_data(id, region, place_type, region_money):
+    kakao = kakao_selenium.kakaoCrawler()
+    data = kakao.place_detail_crawl(id)
+    db, created = Place.objects.update_or_create(
+        kakao_place_id=data['kakao_place_id'])
+    db.region = region
+    db.place_type = place_type
+    db.region_money = region_money
+
+    # crawl data input
+    db.title = data['title']
+    db.category = data['category']
+    db.location = data['location']
+    db.businessHours = data['businessHours']
+    for menu in data['menu']:
+        m, created = Menu.objects.get_or_create(name=menu['name'])
+        m.price = menu['price']
+        db.menu.add(m)
+    db.menu = data['menu']
+    db.save()
